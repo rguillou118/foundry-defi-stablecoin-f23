2ca92b80763a0780d72d5ee01a836f0c81ef2507
rguillou118 <rguillou118@gmail.com>
2023-08-05 20:29:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+1. Relative Stability: Anchored or Pegged -> $1.00
+   1. ChainlinkPrice Feed
+   2. Set a function to exchange ETH&BTC -> $$$
+2. Stability Mecanism (Minting): Algorithmic (Decentralized)
+   1. People can only mint the stablecoin with enough collateral (coded)
+3. Collateral: Exogenous (Crypto)
+   1. wETH
+   2. wBTC
+
+- calculate Health Factor function
+- set Health Factor if debt is 0
+- Added view functions
+
+1. What are the invariants/properties ?
+
+$$
+Some proper oracle use.
+Write more tests.
+Some smart contract audit preparation.
+$$
---
+++ test/fuzz/Invariants.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+
+// Invariants:
+// 1. Total supply of DSC less than Total value of collateral
+// 2. Getter view functions should nver revert
+
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "./Handler.t.sol";
+
+contract Invariants is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    address weth;
+    address wbtc;
+    Handler handler;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+        // targetContract(address(dsce));
+        handler = new Handler(dsce, dsc);
+        targetContract(address(handler));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        // get the value of all the collateral in the protocol
+        // compare it to all the debt (dsc)
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
+
+        console.log("wethValue: ", wethValue);
+        console.log("wbtcValue: ", wbtcValue);
+        console.log("total suplly:", totalSupply);
+        console.log("Times mint called: ", handler.timesMintIsCalled());
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+
+    function invariant_gettersShouldNoRevert() public view {
+        dsce.getLiquidationBonus();
+        dsce.getPrecision;
+    }
+}
